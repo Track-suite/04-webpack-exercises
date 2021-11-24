9d25b1f07129b670b1e0fbe5f0a0d80ca17c3762
tracksuite <youngunner1998@gmail.com>
2021-11-24 15:18:41 +0530

    added some comments and coded the lesson 5

--- src/script.js
+++ src/script.js
@@ -3,14 +3,34 @@ import * as THREE from 'three'
 
 console.log(THREE)
 
+
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Axes Helper
+ */
+ const axesHelper = new THREE.AxesHelper(2)
+ scene.add(axesHelper)
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+//adding position method 1
+// mesh.position.x = 0.12
+// mesh.position.y = 0.2
+// mesh.position.z = 1
+mesh.position.set(0.7, - 0.6, 1)//changing the position method2
+
+// mesh.scale.x = 2 setting the scale method 1 
+// mesh.scale.y = 0.25
+// mesh.scale.z = 0.5 
+mesh.scale.set(2,0.25,0.5)// setting the scale method 2 
+
+console.log(mesh.position.length())
+console.log(mesh.position.normalize())
 
 // Sizes
 const sizes = {
@@ -22,6 +42,7 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+console.log(mesh.position.distanceTo(camera.position))
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
